13013573170c967b04c1d2dd903c99832f538fa0
bigas4441 <bigas@jeit.co.kr>
2022-05-23 17:47:31 +0900

    update

---
+++ dist/js/about-legacy.92430723.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpwa_test_2"]=self["webpackChunkpwa_test_2"]||[]).push([[443],{66:function(t,e,s){s.r(e),s.d(e,{default:function(){return c}});var u=function(){var t=this,e=t.$createElement,s=t._self._c||e;return s("div",{staticClass:"about"},[s("h1",[t._v("This is an about page "+t._s(t.$route.meta["test"]))])])},a=[],n=s(1001),l={},r=(0,n.Z)(l,u,a,!1,null,null,null),c=r.exports}}]);
+//# sourceMappingURL=about-legacy.92430723.js.map
